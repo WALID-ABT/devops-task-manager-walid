d04752ac235145e40fa6bb812e53a5ac1304c87a
Walid El Abbouti <walidelabbouti2005@gmail.com>
2026-02-18 23:39:05 +0000

    Final update to welcome message for Lab 1

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-res.json({ message: "DevOps Task Manager API is running (Lab 1)" });
+res.json({ message: "DevOps Task Manager API is running (Lab 1 Completed)" });
 });
 
 app.get('/tasks', (req, res) => {
